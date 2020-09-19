853323f50e435a8b104b7b6b472334ef5bd9c83a
Takahiro.Hori <brightholidoy610@gmail.com>
2020-09-19 12:09:31 +0000

    task finish01

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :favorites
+  has_many :reverse_of_favorite, class_name: 'Favorite', foreign_key: 'user_id'
+  has_many :favorite_users, through: :reverse_of_favorite, source: :user
 
   validates :content, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,11 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
-  has_many :favorites
-  has_many :microposts
+
+  has_many :favorites #課題
+  has_many :microposts #課題
+  has_many :favorite_microposts, through: :favorites, source: :micropost #課題
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
@@ -31,6 +34,7 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
   #課題
   def favorite(other_micropost)
     unless self.microposts == other_micropost
@@ -44,6 +48,6 @@ class User < ApplicationRecord
   end
   
   def favorite?(other_micropost)
-    self.favorites.include?(other_micropost)
+    self.favorite_microposts.include?(other_micropost)
   end
 end
